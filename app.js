21cebe0a152e7f26f231e780f33c8c25ad23593e
muhammad bin ahmad <dustobalok000@gmail.com>
2023-07-27 22:58:37 +0600

    buyer details functionality added by buyer-details-input through submit btn

---
+++ app.js
@@ -0,0 +1,9 @@
+const detailsButton = document.getElementById('detail-submit-btn');
+
+
+detailsButton.addEventListener('click', function () {
+
+    const buyerDetails = document.getElementById('buyer-details-input');
+    document.getElementById('buyer-info').innerText = buyerDetails.value;
+    buyerDetails.value = '';
+})
